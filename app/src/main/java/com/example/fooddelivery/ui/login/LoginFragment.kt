977b5ba250f2489efe717f9595b4784e0352e845
Kaan Sarıkaya <kadirkaansarikaya@gmail.com>
2021-08-16 17:46:24 +0300

    login design v1.1

--- app/src/main/java/com/example/fooddelivery/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/fooddelivery/ui/login/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.fooddelivery.R
 import com.example.fooddelivery.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,7 +23,16 @@ class LoginFragment : Fragment() {
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        initViews()
+
         return view
+    }
+
+    private fun initViews() {
+        binding.signUpButton.setOnClickListener {
+        }
+
     }
 
     override fun onDestroyView() {
