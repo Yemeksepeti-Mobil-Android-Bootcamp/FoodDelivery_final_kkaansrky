87c98129394af84ad013718160b31b81efb86ae8
Kaan Sarıkaya <kadirkaansarikaya@gmail.com>
2021-08-16 15:14:20 +0300

    login design v1

---
+++ app/src/main/java/com/example/fooddelivery/ui/splash/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.example.fooddelivery.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fooddelivery.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
