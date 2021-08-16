8cea56cc0d9fff11a9b5e63cd59079514d4c1327
Kaan Sarıkaya <kadirkaansarikaya@gmail.com>
2021-08-16 17:55:05 +0300

    change splashGraph and login to main navigate

--- app/src/main/java/com/example/fooddelivery/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/fooddelivery/ui/login/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.example.fooddelivery.ui.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.fooddelivery.R
 import com.example.fooddelivery.databinding.FragmentLoginBinding
+import com.example.fooddelivery.ui.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -30,7 +32,15 @@ class LoginFragment : Fragment() {
     }
 
     private fun initViews() {
+
+        binding.loginButton.setOnClickListener{
+            val intent = Intent(requireActivity(),MainActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
+
         binding.signUpButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
 
     }
