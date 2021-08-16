972a566f07b28c1526e8df1a8e7ca1d13bddc20b
Kaan Sarıkaya <kadirkaansarikaya@gmail.com>
2021-08-16 16:02:41 +0300

    Splash design v1

--- app/src/main/java/com/example/fooddelivery/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/fooddelivery/ui/splash/SplashFragment.kt
@@ -1,13 +1,19 @@
 package com.example.fooddelivery.ui.splash
 
+import android.animation.Animator
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.fooddelivery.R
+import com.example.fooddelivery.databinding.ActivitySplashBinding
 import com.example.fooddelivery.databinding.FragmentSplashBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SplashFragment : Fragment() {
     private var _binding: FragmentSplashBinding? = null
     
@@ -20,7 +26,24 @@ class SplashFragment : Fragment() {
     ): View {
         _binding = FragmentSplashBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        initViews()
+
         return view
+    }
+
+    private fun initViews() {
+        binding.splashAnimation.addAnimatorListener(object: Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator?) {}
+
+            override fun onAnimationEnd(animation: Animator?) {
+                findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {}
+
+            override fun onAnimationRepeat(animation: Animator?) {}
+        })
     }
 
     override fun onDestroyView() {
