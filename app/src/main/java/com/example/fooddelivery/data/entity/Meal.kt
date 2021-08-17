f2c72fc227373da7bb42e5664cf6ad89f9c63b18
Kaan Sarıkaya <kadirkaansarikaya@gmail.com>
2021-08-17 12:08:52 +0300

    add Restaurant and Meal Object

---
+++ app/src/main/java/com/example/fooddelivery/data/entity/Meal.kt
@@ -0,0 +1,18 @@
+package com.example.fooddelivery.data.entity
+
+import com.google.gson.annotations.SerializedName
+
+data class Meal(
+    @SerializedName("_id")
+    val id: String,
+    @SerializedName("description")
+    val description: String,
+    @SerializedName("imageUrl")
+    val image: String,
+    @SerializedName("ingredients")
+    val ingredients: ArrayList<String>,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("price")
+    val price: String
+)
---
+++ app/src/main/java/com/example/fooddelivery/data/entity/Restaurant.kt
@@ -0,0 +1,32 @@
+package com.example.fooddelivery.data.entity
+
+import com.google.gson.annotations.SerializedName
+
+data class Restaurant(
+    @SerializedName("_id")
+    val id: String,
+    @SerializedName("deliveryInfo")
+    val deliveryInfo: String,
+    @SerializedName("deliveryTime")
+    val deliveryTime: String,
+    @SerializedName("imageUrl")
+    val image: String,
+    @SerializedName("address")
+    val address: String,
+    @SerializedName("district")
+    val district: String,
+    @SerializedName("meals")
+    val meals: ArrayList<Meal>,
+    @SerializedName("minDeliveryFee")
+    val minimumDeliveryFee: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("paymentMethods")
+    val paymentMethods: String,
+    @SerializedName("phone")
+    val phone: String,
+    @SerializedName("website")
+    val website: String,
+    @SerializedName("cuisine")
+    val cuisine: String,
+)
